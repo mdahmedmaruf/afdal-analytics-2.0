9e189b1818c4de9a95ded7ef09de2c7598d6e6ef
mdahmedmaruf <mdahmedmaruf@gmail.com>
2023-08-06 13:35:53 +0600

    add dropdown

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 const togglePassword = document.querySelector('#togglePassword');
 const password = document.querySelector('#password');
+const eyeImages = document.querySelectorAll('.open-eye-img');
 
 togglePassword.addEventListener('click', function (e) {
 	// toggle the type attribute
@@ -7,5 +8,16 @@ togglePassword.addEventListener('click', function (e) {
 		password.getAttribute('type') === 'password' ? 'text' : 'password';
 	password.setAttribute('type', type);
 	// toggle the eye slash icon
-	//this.classList.toggle('visually-hidden');
+	// if (this.classList.contains('visually-hidden')) {
+	// 	this.classList.remove('visually-hidden');
+	// } else {
+	// 	this.classList.add('visually-hidden');
+	// }
+	for (const eyeImage of eyeImages) {
+		if (eyeImage.classList.contains('visually-hidden')) {
+			eyeImage.classList.remove('visually-hidden');
+		} else {
+			eyeImage.classList.add('visually-hidden');
+		}
+	}
 });
